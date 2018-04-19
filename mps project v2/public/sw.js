2b7c6ba3baa8cf9144da0f8baac3c984db1591c8
Youming Luo <yl2397@cornell.edu>
2018-04-19 17:47:17 -0400

    clean for real project

--- mps project v2/public/sw.js
+++ mps project v2/public/sw.js
@@ -7,7 +7,7 @@ self.addEventListener('activate',function(event){
     return self.clients.claim();
 });
 
-// self.addEventListener('fetch',function(event){
-//     console.log('service worker fetch',event);
-// });
+self.addEventListener('fetch',function(event){
+    console.log('service worker fetch',event);
+});
 
