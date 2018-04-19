251e6eca44873b13a2b0020ec4f3b1945e415bce
Youming Luo <yl2397@cornell.edu>
2018-04-19 17:31:59 -0400

    add support for legacy broswer of fetch and promise

--- mps project v2/public/src/js/app.js
+++ mps project v2/public/src/js/app.js
@@ -1,7 +1,15 @@
+if(!window.Promise){
+    window.Promise = Promise;
+}
+
+
 if('serviceWorker' in navigator){
     navigator.serviceWorker
     .register('/sw.js')
     .then(function(){
         console.log("service worker registered!")
+    })
+    .catch(function(err){
+        console.log(err);
     });
 }
