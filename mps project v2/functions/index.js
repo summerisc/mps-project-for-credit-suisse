082dc6ffa6ac71ecadabac0fbb929ad2633acb2d
Youming Luo <yl2397@cornell.edu>
2018-04-22 16:05:02 -0400

    end of section 10

--- mps project v2/functions/index.js
+++ mps project v2/functions/index.js
@@ -1,6 +1,7 @@
 var functions = require('firebase-functions');
 var admin = require('firebase-admin');
 var cors = require('cors')({origin:true});
+var webpush = require('web-push');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -20,6 +21,28 @@ exports.storePostData = functions.https.onRequest(function(request, response) {
         image: request.body.image
       })
         .then(function() {
+          webpush.setVapidDetails('mailto:mpspwa@gmail.com', 'BCzdZQjv6ytspvXEQKcdkfGu-VlowSoyLsiMy6-bIM3Iv5vqD6yGvRHccNxnoHc5ofvLtH98E46EBVsp8Il6xMk', 'hCkjU47WBwQt40f2iApbvuIRgpew2Ze7lGdcOCEW2Yc');
+          return admin.database().ref('subscriptions').once('value');
+        })
+        .then(function(subscriptions){
+          subscriptions.forEach(function (sub) {
+            var pushConfig = {
+              endpoint: sub.val().endpoint,
+              keys: {
+                auth: sub.val().keys.auth,
+                p256dh: sub.val().keys.p256dh
+              }
+            };
+  
+            webpush.sendNotification(pushConfig, JSON.stringify({
+              title: 'New Post', 
+              content: 'New Post added!',
+              openUrl: '/help'
+            }))
+              .catch(function(err) {
+                console.log(err);
+              })
+          });
           response.status(201).json({message: 'Data stored', id: request.body.id});
         })
         .catch(function(err) {
--- mps project v2/public/src/js/app.js
+++ mps project v2/public/src/js/app.js
@@ -1,15 +1,105 @@
-if(!window.Promise){
-    window.Promise = Promise;
+var enableNotificationsButtons = document.querySelectorAll('.enable-notifications');
+
+if (!window.Promise) {
+  window.Promise = Promise;
 }
 
-
-if('serviceWorker' in navigator){
-    navigator.serviceWorker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
     .register('/sw.js')
-    .then(function(){
-        console.log("service worker registered!")
+    .then(function () {
+      console.log('Service worker registered!');
     })
-    .catch(function(err){
-        console.log(err);
+    .catch(function(err) {
+      console.log(err);
     });
+}
+function displayConfirmNotification() {
+    if ('serviceWorker' in navigator) {
+      var options = {
+        body: 'You successfully subscribed to our Notification service!',
+        icon: '/src/images/icons/app-icon-96x96.png',
+        image: '/src/images/sf-boat.jpg',
+        dir: 'ltr',
+        lang: 'en-US', // BCP 47,
+        vibrate: [100, 50, 200],
+        badge: '/src/images/icons/app-icon-96x96.png',
+        tag: 'confirm-notification',
+        renotify: true,
+        actions: [
+          { action: 'confirm', title: 'Okay', icon: '/src/images/icons/app-icon-96x96.png' },
+          { action: 'cancel', title: 'Cancel', icon: '/src/images/icons/app-icon-96x96.png' }
+        ]
+      };
+  
+      navigator.serviceWorker.ready
+        .then(function(swreg) {
+          swreg.showNotification('PoC successfully subscribed!', options);
+        });
+    }
+  }
+
+
+  function configurePushSub() {
+    if (!('serviceWorker' in navigator)) {
+      return;
+    }
+  
+    var reg;
+    navigator.serviceWorker.ready
+      .then(function(swreg) {
+        reg = swreg;
+        return swreg.pushManager.getSubscription();
+      })
+      .then(function(sub) {
+        if (sub === null) {
+          // Create a new subscription
+          var vapidPublicKey = 'BCzdZQjv6ytspvXEQKcdkfGu-VlowSoyLsiMy6-bIM3Iv5vqD6yGvRHccNxnoHc5ofvLtH98E46EBVsp8Il6xMk';
+          var convertedVapidPublicKey = urlBase64ToUint8Array(vapidPublicKey);
+          return reg.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: convertedVapidPublicKey
+          });
+        } else {
+          // We have a subscription
+        }
+      })
+      .then(function(newSub) {
+        return fetch('https://pwa-final-81313.firebaseio.com/subscriptions.json', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          body: JSON.stringify(newSub)
+        })
+      })
+      .then(function(res) {
+        if (res.ok) {
+          displayConfirmNotification();
+        }
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  }
+
+
+function askForNotificationPermission() {
+  Notification.requestPermission(function(result) {
+    console.log('User Choice', result);
+    if (result !== 'granted') {
+      console.log('No notification permission granted!');
+    } else {
+        //displayConfirmNotification();
+        configurePushSub();
+    }
+  });
+}
+
+if ('Notification' in window && 'serviceWorker' in navigator) {
+  for (var i = 0; i < enableNotificationsButtons.length; i++) {
+    enableNotificationsButtons[i].style.display = 'inline-block';
+    enableNotificationsButtons[i].addEventListener('click', askForNotificationPermission);
+  }
 }
